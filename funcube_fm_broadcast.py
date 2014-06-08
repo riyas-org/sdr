6c773ea07423b3845b502fe3bc2cd52269549fa3
sinu <sinu@sinu-1001PX.(none)>
2014-06-08 13:53:47 +0200

    updated with new grc on jun 2014

--- funcube_fm_broadcast.py
+++ funcube_fm_broadcast.py
@@ -4,18 +4,18 @@
 # Title: Simple test for funcube
 # Author: Riyas
 # Description: Test the Funcube pro with wide band fm BC-Riyas
-# Generated: Wed Dec 25 12:52:48 2013
+# Generated: Sun Jun  8 13:51:58 2014
 ##################################################
 
+from gnuradio import analog
 from gnuradio import audio
-from gnuradio import blks2
-from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import fcd
 from gnuradio import gr
-from gnuradio import window
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
-from gnuradio.gr import firdes
+from gnuradio.fft import window
+from gnuradio.filter import firdes
 from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import waterfallsink2
 from grc_gnuradio import wxgui as grc_wxgui
@@ -24,81 +24,89 @@ import wx
 
 class funcube_fm_broadcast(grc_wxgui.top_block_gui):
 
-	def __init__(self):
-		grc_wxgui.top_block_gui.__init__(self, title="Simple test for funcube")
-		_icon_path = "/home/risu/.local/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
-		self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Simple test for funcube")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
-		##################################################
-		# Variables
-		##################################################
-		self.samp_rate = samp_rate = 192000
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 192000
 
-		##################################################
-		# Blocks
-		##################################################
-		self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
-			self.GetWin(),
-			baseband_freq=0,
-			dynamic_range=100,
-			ref_level=0,
-			ref_scale=2.0,
-			sample_rate=samp_rate,
-			fft_size=512,
-			fft_rate=15,
-			average=False,
-			avg_alpha=None,
-			title="Waterfall Plot",
-		)
-		self.Add(self.wxgui_waterfallsink2_0.win)
-		self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
-			self.GetWin(),
-			baseband_freq=0,
-			y_per_div=10,
-			y_divs=10,
-			ref_level=0,
-			ref_scale=2.0,
-			sample_rate=samp_rate,
-			fft_size=1024,
-			fft_rate=15,
-			average=False,
-			avg_alpha=None,
-			title="FFT Plot",
-			peak_hold=False,
-		)
-		self.Add(self.wxgui_fftsink2_0.win)
-		self.fcd_source_c_0 = fcd.source_c("hw:1")
-		self.fcd_source_c_0.set_freq(145500000)
-		    
-		self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "/home/risu/Desktop/fcd_capture_sample.raw")
-		self.blocks_file_sink_0.set_unbuffered(False)
-		self.blks2_wfm_rcv_0 = blks2.wfm_rcv(
-			quad_rate=192000,
-			audio_decimation=4,
-		)
-		self.audio_sink_0 = audio.sink(48000, "", True)
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="Waterfall Plot",
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
+        self.fcd_source_c_0 = fcd.source_c("hw:1")
+        self.fcd_source_c_0.set_freq(145500000)
+            
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=192000,
+        	audio_decimation=4,
+        )
 
-		##################################################
-		# Connections
-		##################################################
-		self.connect((self.fcd_source_c_0, 0), (self.blks2_wfm_rcv_0, 0))
-		self.connect((self.blks2_wfm_rcv_0, 0), (self.audio_sink_0, 0))
-		self.connect((self.fcd_source_c_0, 0), (self.wxgui_fftsink2_0, 0))
-		self.connect((self.fcd_source_c_0, 0), (self.blocks_file_sink_0, 0))
-		self.connect((self.fcd_source_c_0, 0), (self.wxgui_waterfallsink2_0, 0))
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.fcd_source_c_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.fcd_source_c_0, 0), (self.wxgui_waterfallsink2_0, 0))
+        self.connect((self.fcd_source_c_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
 
 
-	def get_samp_rate(self):
-		return self.samp_rate
+# QT sink close method reimplementation
 
-	def set_samp_rate(self, samp_rate):
-		self.samp_rate = samp_rate
-		self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
-		self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
 
 if __name__ == '__main__':
-	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-	(options, args) = parser.parse_args()
-	tb = funcube_fm_broadcast()
-	tb.Run(True)
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = funcube_fm_broadcast()
+    tb.Start(True)
+    tb.Wait()
 
